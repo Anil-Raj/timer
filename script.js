230ba5ebe22f525fd71cddbd98a864b078bddd45
Anil G <anil.gangadharaiah@neudesic.com>
2018-07-30 01:57:25 +0530

    init

--- script.js
+++ script.js
@@ -1,115 +1,96 @@
 var timer = {
-    time:20,
-    laps:[],
-    started:false,
-    clock:null
-}
+  duration: 0,
+  time: 0,
+  lap: 0,
+  isCountUp: 0,
+  lapCount: 0
+};
 
-lap = function(){
-    lap_no = timer.laps.length+1;
-    console.log('lap'+ lap_no,"time"+timer.time);
-    timer.laps.push({name:"Lap"+ lap_no,time:timer.time});
-    var ul = document.getElementById("myUL");
-  var li = document.createElement("li");
-    // var t = document.createTextNode(lap_no);
-    // t.appendChild("Lap".sup());
-    // h.appendChild(t);
-    // document.body.appendChild(h);
-    var number = document.createElement("div");
-number.innerHTML = lap_no+"<sup>"+"Lap"+"</sup> "+timer.time;
-var textnode = document.createTextNode(lap_no+"<sup>"+"Lap"+"</sup> "+timer.time);
-li.appendChild(number);
+toMinSec = function(time) {
+  return parseInt(Math.abs(time / 60)) + ":" + Math.abs(time % 60);
+};
 
-ul.insertBefore(li, ul.childNodes[0]);
-//   ul.appendChild(li);
-}
-down = function(){
-    console.log('started');
-    document.getElementById('up').style.display="none";
-    document.getElementById('down').style.display="none";
+start = function(option) {
+  timer.isCountUp = option;
+  updateCountUpDownButton("none");
+  updateResumeCancelButton("none");
+  updateLapPauseButton("inline-block");
+  document.getElementById("timer").style.display = "block";
+  document.getElementById("time-input").style.display = "none";
+  timer.duration = document.getElementById("time-input").value;
+  time = timer.isCountUp
+    ? toMinSec(timer.time)
+    : toMinSec(timer.duration - timer.time);
+  document.getElementById("timer").innerHTML = time;
+  timer.clock = new IntervalTimer(updateTimerUI, 1000);
+};
+
+updateTimerUI = function() {
+  timer.time = timer.time + 1;
+  console.log(timer.isCountUp, toMinSec(timer.time), toMinSec(timer.duration - timer.time));
+  
+  time = timer.isCountUp
+    ? toMinSec(timer.time)
+    : toMinSec(timer.duration - timer.time);
+    console.log(time);
     
-    document.getElementById('lap').style.display="inline-block";
-    document.getElementById('pause').style.display="inline-block";
-    document.getElementById('time-input').style.display="none";
-    document.getElementById('timer').style.display="block";
-    aa =     document.getElementById('time-input');
-    console.log(aa);
-    timer.time = aa.value;
-    document.getElementById('timer').innerHTML = timer.time; 
-    timer.clock =  setInterval(function(){
-        document.getElementById('timer').innerHTML = timer.time; 
-        timer.time= timer.time - 1;
-        if(timer.time < 0){
-            clearInterval(timer.clock);
-            cancelStyle();
-            timer.time = 0; 
-        }
-    }, 1000);
-}
-up = function(){
-    console.log('started');
-    document.getElementById('up').style.display="none";
-    document.getElementById('down').style.display="none";
-    
-    document.getElementById('lap').style.display="inline-block";
-    document.getElementById('pause').style.display="inline-block";
-    document.getElementById('time-input').style.display="none";
-    document.getElementById('timer').style.display="block";
-    aa =     document.getElementById('time-input');
-    console.log(aa);
+  document.getElementById("timer").innerHTML = time;
+  if (timer.duration - timer.time < 1) {
+    timer.clock.pause();
+    updateCountUpDownButton("inline-block");
+  updateResumeCancelButton("none");
+  updateLapPauseButton("none");
     timer.time = 0;
-    document.getElementById('timer').innerHTML = timer.time; 
-    timer.clock =  setInterval(function(){
-        document.getElementById('timer').innerHTML = timer.time; 
-        timer.time= timer.time + 1;
-        if(timer.time > aa.value){
-            clearInterval(timer.clock);
-            cancelStyle();
-        }
-    }, 1000);
-}
+  }
+};
 
-timerFunc = function(){  
-    document.getElementById('timer').innerHTML = timer.time; 
-    timer.time= timer.time - 1;
-}
+pause = function() {
+  timer.clock.pause();
+  document.getElementById("resume").style.display = "inline-block";
+  document.getElementById("cancel").style.display = "inline-block";
+  updateLapPauseButton("none");
+};
 
-pause = function(){
-    clearInterval(timer.clock);
-    document.getElementById('resume').style.display="inline-block";
-    document.getElementById('cancel').style.display="inline-block";
-    document.getElementById('lap').style.display="none";
-    document.getElementById('pause').style.display="none";
-    
-}
+resume = function() {
+  timer.clock = new IntervalTimer(updateTimerUI, 1000);
+  updateCountUpDownButton("none");
+  updateResumeCancelButton("none");
+  updateLapPauseButton("inline-block");
+  document.getElementById("timer").style.display = "block";
+  document.getElementById("time-input").style.display = "none";
+};
 
-resume = function(){
-    timer.clock =  setInterval(function(){
-        document.getElementById('timer').innerHTML = timer.time; 
-        timer.time= timer.time - 1;
-        if(timer.time < 1){
-            clearInterval(timer.clock);
-            cancelStyle();
-        }
-    }, 1000);
-    document.getElementById('lap').style.display="inline-block";
-    document.getElementById('pause').style.display="inline-block";
-    document.getElementById('resume').style.display="none";
-    document.getElementById('cancel').style.display="none";
-}
-cancel = function(){
-    clearInterval(timer.clock);
-    cancelStyle();
+cancel = function() {
+    updateCountUpDownButton("inline-block");
+    updateResumeCancelButton("none");
+    updateLapPauseButton("none");
+    document.getElementById("time-input").style.display = "block";
+    document.getElementById("timer").style.display = "none";
     timer.time = 20;
-}
+};
+  
+lap = function() {
+    var ul = document.getElementById("myUL");
+    var li = document.createElement("li");
+    var number = document.createElement("div");
+    timer.lapCount = timer.lapCount + 1;
+    number.innerHTML = timer.lapCount +"<sup>" +"Lap" +"</sup> " + toMinSec(timer.time - timer.lap);
+    li.appendChild(number);
+    ul.insertBefore(li, ul.childNodes[0]);
+    timer.lap = timer.time;
+};
 
-cancelStyle= function(){
-    document.getElementById('lap').style.display="none";
-            document.getElementById('pause').style.display="none";
-            document.getElementById('up').style.display="inline-block";
-            document.getElementById('down').style.display="inline-block";
-            document.getElementById('time-input').style.display="block";
-            document.getElementById('timer').style.display="none";
-            document.getElementById('resume').style.display="none";
-            document.getElementById('cancel').style.display="none";
-}
+updateResumeCancelButton = function(style) {
+  document.getElementById("resume").style.display = style;
+  document.getElementById("cancel").style.display = style;
+};
+
+updateCountUpDownButton = function(style) {
+  document.getElementById("up").style.display = style;
+  document.getElementById("down").style.display = style;
+};
+
+updateLapPauseButton = function(style) {
+  document.getElementById("lap").style.display = style;
+  document.getElementById("pause").style.display = style;
+};
